633bbad5928a6010da3fd3354823b5fd8d64241f
Kevin <kevindana99@gmail.com>
2021-07-20 11:43:36 -0700

    cambios rutas

--- models/Rutas.js
+++ models/Rutas.js
@@ -2,7 +2,7 @@ const RUTA_SERVER = 'https://appwebbers.000webhostapp.com/fetch/server.php';
 const RUTA_PAGOS = 'https://appwebbers.000webhostapp.com/fetch/models/Preferencia_pagos.php';
 //export const RUTA_SERVER = 'fetch/server.php';
 
-export default {
+export {
 
     RUTA_SERVER,
     RUTA_PAGOS
